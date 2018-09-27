faffdef2d9ab613197d17a5ec15e95f26c066660
dmit2015 <dmit2015@ubuntu>
2018-09-27 16:21:26 -0600

    -commit

--- dmit2015-assignment01-Hang/src/main/java/dmit2015/controller/LoanController.java
+++ dmit2015-assignment01-Hang/src/main/java/dmit2015/controller/LoanController.java
@@ -2,12 +2,15 @@ package dmit2015.controller;
 
 import java.io.Serializable;
 
+import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
+import org.omnifaces.util.Messages;
+import org.primefaces.model.chart.Axis;
+import org.primefaces.model.chart.AxisType;
 import org.primefaces.model.chart.BarChartModel;
 import org.primefaces.model.chart.ChartSeries;
-import org.primefaces.model.chart.HorizontalBarChartModel;
 
 import dmit2015.model.Loan;
 import dmit2015.model.LoanSchedule;
@@ -39,43 +42,68 @@ public class LoanController implements Serializable{
 	public void setCurrentLoan(Loan currentLoan) {
 		this.currentLoan = currentLoan;
 	}
-	
 	public void calculate() {
-		createBarModel();
+		
+		createBarModels();
 		getLoanScheduleArray();
-		
-		
 	}
-	
-	
 
-	private BarChartModel loanChart = new BarChartModel();
+	private BarChartModel barModel;
 	
-	
-
-	private BarChartModel initBarModel() {
-		BarChartModel model = new BarChartModel();
-		LoanSchedule[] currentLoanScheduleArray = currentLoan.getLoanScheduleArray();
-		ChartSeries remainingBalance = new ChartSeries();
-		remainingBalance.setLabel("Remaining Balance");
-		for(int i=0; i< currentLoan.getAmortizationPeriod();i++) {
-			remainingBalance.set(i+1, currentLoanScheduleArray[i * 12 + 11].getRemainingBalance());	
-		}
-		
-		model.addSeries(remainingBalance);
-		
-		
-		return model;
+	@PostConstruct
+	public void init() {
+        createBarModels();
     }
-	
-	private void createBarModel() {
-		loanChart = initBarModel();
-		
-		LoanSchedule[] currentLoanScheduleArray = currentLoan.getLoanScheduleArray();
-		
-		
-	}
-	
+ 
+    public BarChartModel getBarModel() {
+        return barModel;
+    }
+     
+ 
+    private BarChartModel initBarModel() {
+        BarChartModel model = new BarChartModel();
+        LoanSchedule[] currentLoanScheduleArray = currentLoan.getLoanScheduleArray();
+        ChartSeries remainingBalance = new ChartSeries();
+        remainingBalance.setLabel("Remaining Balance");
+        for(int i=0; i <currentLoan.getAmortizationPeriod();i++) {
+        	remainingBalance.set(i+1, currentLoanScheduleArray[i*12 +11].getRemainingBalance());
+        }    
+ 
+        model.addSeries(remainingBalance);
+
+         
+        return model;
+    }
+     
+    private void createBarModels() {
+        createBarModel();
+    }
+     
+    private void createBarModel() {
+    	
+        barModel = initBarModel();
+         
+        LoanSchedule[] currentLoanScheduleArray = currentLoan.getLoanScheduleArray();
+        ChartSeries remainingBalance = new ChartSeries();
+        remainingBalance.setLabel("Remaining Balance");
+        for(int i=0; i <currentLoan.getAmortizationPeriod();i++) {
+        	remainingBalance.set(i+1, currentLoanScheduleArray[i*12 +11].getRemainingBalance());
+        }    
+        
+        
+        barModel.setTitle("Mortgage Amortizaion Payment");
+        barModel.setLegendPosition("ne");
+         
+        Axis xAxis = barModel.getAxis(AxisType.X);
+        xAxis.setMax(currentLoan.getAmortizationPeriod());
+        xAxis.setLabel("Years");
+         
+        Axis yAxis = barModel.getAxis(AxisType.Y);
+        yAxis.setMin(0);
+        yAxis.setMax(currentLoan.getMortgageAmount());
+    }
+     
+    
 	
 
 }
