8d657f291f983d4daac1f25fa3b59177552b0716
dmit2015 <dmit2015@ubuntu>
2018-10-17 20:16:06 -0600

    .

--- dmit2015-assignment02-start/src/main/java/dmit2015/hr/controller/JobDeleteController.java
+++ dmit2015-assignment02-start/src/main/java/dmit2015/hr/controller/JobDeleteController.java
@@ -1,5 +1,82 @@
 package dmit2015.hr.controller;
+import java.io.Serializable;
+import javax.enterprise.inject.Produces;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.validation.constraints.NotNull;
 
-public class JobDeleteController {
+import org.omnifaces.util.Faces;
+import org.omnifaces.util.Messages;
 
+import dmit2015.hr.entity.Job;
+import dmit2015.hr.service.HumanResourceService;
+
+@Named
+@ViewScoped
+public class JobDeleteController implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Inject
+	private HumanResourceService currentHumanResourceService;
+	
+	@Produces
+	@Named
+	private Job existingJob;
+	
+	
+	@NotNull(message = "Search value is required.")
+	private String idQueryValue;
+	
+	public String getidQueryValue() {
+		return idQueryValue;
+	}
+	
+	public void setIdQueryValue(String idQueryValue) {
+		this.idQueryValue = idQueryValue;
+	}
+	
+	public void findJob() {
+		try {
+			existingJob = currentHumanResourceService.findOneJob(idQueryValue);
+			if(existingJob !=null) {
+				Messages.addGlobalInfo("Query successful");
+			}else {
+				Messages.addGlobalError("Query unsuccessful");
+		}
+		}catch (Exception e) {
+			Messages.addGlobalError("Query unsucessful");
+			Messages.addGlobalError("{0}", e.getMessage());	
+		}
+		
+	}
+	
+	public String deleteJob() {
+		String nextPage = null;
+		try {
+			currentHumanResourceService.deleteJob(existingJob);
+			existingJob =null;
+			idQueryValue = null;
+			Messages.addGlobalInfo("Delete successful");
+			nextPage = "viewShippers?faces-redirect=true";
+		}catch (Exception e) {
+			Messages.addGlobalInfo("Delete unsuccessful");
+			Messages.addGlobalError("{0}", e.getMessage());
+		}
+		return nextPage;
+	}
+	
+	public void cancel() {
+		existingJob = null;
+		idQueryValue = null;
+	}
+	
+	
+	public void findByQueryParameterId() {
+		if (!Faces.isPostback() && !Faces.isValidationFailed() ) {
+			if (idQueryValue != null && existingJob == null) {
+				findJob();		
+			}
+		}
+	}
 }
--- dmit2015-assignment02-start/src/main/java/dmit2015/hr/service/HumanResourceService.java
+++ dmit2015-assignment02-start/src/main/java/dmit2015/hr/service/HumanResourceService.java
@@ -5,8 +5,10 @@ import java.util.List;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.validation.constraints.NotNull;
 
 import dmit2015.hr.entity.Job;
+import dmit2015.hr.entity.Location;
 @Stateless
 public class HumanResourceService {
 
@@ -22,6 +24,33 @@ public class HumanResourceService {
 
 	public void addJob(Job newJob) {
 		entityManager.persist(newJob);
+		
+	}
+
+
+	public Job findOneJob(@NotNull(message = "Search value is required.") String idQueryValue) {
+		
+		return entityManager.find(Job.class, idQueryValue);
+	}
+
+
+	public void updateJob(Job existingJob) {
+		entityManager.merge(existingJob);
+		
+	}
+
+
+	public void deleteJob(Job existingJob) throws Exception{
+		existingJob = entityManager.merge(existingJob);
+		if(existingJob.getEmployees().size()>0) {
+			throw new Exception("You are not allowed to delete a Job with existing employees.");
+		}
+		entityManager.remove(existingJob);
+	}
+
+
+	public void addLocation(Location newLocation) {
+		entityManager.persist(newLocation);
 		
 	}
 
